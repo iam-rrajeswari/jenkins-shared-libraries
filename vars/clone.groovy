d31347d5bc97249e5c35ed60b4457209bd42b633
iam-rrajeswari <vemularajarajeswari2702@gmail.com>
2024-10-29 22:10:54 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String GitUrl,String GitBranch){
   echo" This is cloning the code"
-  gir url: "${GitUrl}",branch:"GitBranch"
+  gir url: "${GitUrl}",branch:"${GitBranch}"
   echo " Git clone is successful"
 }
